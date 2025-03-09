d4b1b534334a1a2c8c73c204bf0688b90693754b
zehan <zehan9211@gmail.com>
2025-03-09 23:16:12 +0100

    Next output variable has migrated to out

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
